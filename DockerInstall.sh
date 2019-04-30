4b9f71efe4bc9ab7495aa577087c51ce83c1863e
sania454 <sania454@rambler.ru>
2019-04-30 19:07:54 +0300

    Create DockerInstall.sh
    File for Docker-CE installation

---
+++ DockerInstall.sh
@@ -0,0 +1,4 @@
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+echo 'deb https://download.docker.com/linux/debian stretch stable' > /etc/apt/sources.list.d/docker.list
+apt-get update
+apt-get install docker-ce
